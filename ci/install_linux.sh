9654c0590bee0ea33c8ac2117e4edab27cb0aa35
masafumi-deepx <masafumi.takahashi@deepx.co.jp>
2025-01-09 16:00:22 +0900

    [fix] skip install octomap

--- ci/install_linux.sh
+++ ci/install_linux.sh
@@ -5,11 +5,11 @@ sudo apt-get -qq --yes --force-yes install libeigen3-dev
 sudo apt-get -qq --yes --force-yes install libccd-dev
 
 # Octomap
-git clone https://github.com/OctoMap/octomap
-cd octomap
-git checkout tags/v1.8.0
-mkdir build
-cd build
-cmake ..
-make
-sudo make install
+# git clone https://github.com/OctoMap/octomap
+# cd octomap
+# git checkout tags/v1.8.0
+# mkdir build
+# cd build
+# cmake ..
+# make
+# sudo make install
